320f4b719ee179f3ce5e3bb234396c721a823510
Evan Hahn <me@evanhahn.com>
2014-08-19 10:31:33 -0700

    remove a big chunk of code

--- scrape.py
+++ scrape.py
@@ -17,9 +17,7 @@ def get_webdav_urls(username, password):
     login_form['password'].value = password
     browser.submit_form(login_form)
 
-    # get the list of courses
-
-    course_ids = []
+    # get the results
 
     browser.follow_link(browser.find(
         class_='toolMenuLink ',
@@ -27,38 +25,21 @@ def get_webdav_urls(username, password):
     ))
     browser.open(browser.find(class_='portletMainIframe').attrs['src'])
 
+    results = []
+
     course_links = browser.select('#sitesForm td h4 a[target="_top"]')
     for course_link in course_links:
+
         if not course_link.attrs:
             continue
         href = course_link.attrs['href']
         if '~' in href:
             continue
-        course_ids.append(findall('\/[^\/]+$', href)[0][1:])
 
-    # get the webDAV url from every course
-
-    results = []
-
-    for course_id in course_ids:
-
-        browser.open('https://ctools.umich.edu/portal/site/' + course_id)
-
-        resources_link = browser.find(
-            class_='toolMenuLink ',
-            title='For posting documents, website URLs, etc.'
+        results.append(
+            'https://ctools.umich.edu/dav' +
+            findall('\/[^\/]+$', href)[0]
         )
-        if not resources_link:
-            continue
-        browser.follow_link(resources_link)
-
-        browser.open(browser.find(class_='portletMainIframe').attrs['src'])
-        browser.follow_link(browser.find(
-            title='Upload-Download Multiple Resources'
-        ))
-
-        url = browser.select('#davdocs_step1 p.indnt3 strong')[0].text
-        results.append(url)
 
     return results
 
